15c1ec0d55577c494d0de5b86bcf4d0f512224b0
Alexander Rødseth <rodseth@gmail.com>
2012-07-04 14:52:03 +0200

    Added several string functions or tests

--- pystring.go
+++ pystring.go
@@ -11,53 +11,157 @@ package pystring
 import (
 	"errors"
 	"strings"
+	"bytes"
 )
 
 const (
-	ascii_letters   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
-	ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	digits          = "0123456789"
-	hexdigits       = "0123456789abcdefABCDEF"
-	octdigits       = "01234567"
-	punctuation     = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-	printable       = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
-	whitespace      = " \t\n\r\x0b\x0c"
+	ASCII_letters   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ASCII_lowercase = "abcdefghijklmnopqrstuvwxyz"
+	ASCII_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	Digits          = "0123456789"
+	HexDigits       = "0123456789abcdefABCDEF"
+	OctDigits       = "01234567"
+	Punctuation     = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+	Printable       = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
+	Whitespace      = " \t\n\r\x0b\x0c"
 )
 
 type PyString struct {
-	s string
+	text string
 }
 
-func New(s string) *PyString {
-	return &PyString{s}
+func New(text string) *PyString {
+	return &PyString{text}
 }
 
 func (p *PyString) Capitalize() *PyString {
-	p.s = strings.Title(p.s)
+	p.text = strings.Title(p.text)
 	return p
 }
 
 func (p *PyString) Strip() *PyString {
-	p.s = strings.TrimSpace(p.s)
+	p.text = strings.TrimSpace(p.text)
 	return p
 }
 
-func (p *PyString) Count(s string) int {
-	return strings.Count(p.s, s)
+func (p *PyString) Count(text string) int {
+	return strings.Count(p.text, text)
 }
 
 func (p *PyString) Encode() []byte {
-	return []byte(p.s)
+	return []byte(p.text)
 }
 
-func (p *PyString) Index(s string) (int, error) {
+func (p *PyString) Index(text string) (int, error) {
 	var err error
-	i := strings.Index(p.s, s)
+	i := strings.Index(p.text, text)
 	if i == -1 {
 		err = errors.New("Not found")
 	} else {
 		err = nil
 	}
 	return i, err
+}
+
+/* strings.HasSuffix should also work */
+func (p *PyString) EndsWith(text string) bool {
+	startpos := len(p.text) - len(text)
+	if startpos < 0 {
+		return false
+	}
+	for pos := startpos; pos < len(p.text); pos++ {
+		if p.text[pos] != text[pos - startpos] {
+			return false
+		}
+	}
+	return true
+}
+
+/* strings.HasPrefix should also work */
+func (p *PyString) StartsWith(text string) bool {
+	if len(text) > len(p.text) {
+		return false
+	}
+	for pos := 0; pos < len(text); pos++ {
+		if p.text[pos] != text[pos] {
+			return false
+		}
+	}
+	return true
+}
+
+/* Instead of "xyz" in x in Python, use x.Has("xyz") in Go */
+func (p *PyString) Has(text string) bool {
+	return -1 != strings.Index(p.text, text)
+}
+
+/* To fetch the string */
+func (p *PyString) Get() string {
+	return p.text
+}
+
+/* Is the string empty? Instead of if s: ... in Python, use if s.Empty() { ... in Go */
+func (p *PyString) Empty() bool {
+	return 0 == len(p.text)
+}
+
+func (p* PyString) Find(text string) int {
+	return strings.Index(p.text, text)
+}
+
+func (p* PyString) IsDigit() bool {
+	var isDigit bool
+	if p.Empty() {
+		return false
+	}
+	for _, letter := range p.text {
+		isDigit = false
+		for _, digit := range Digits {
+			if letter == digit {
+				isDigit = true
+				break
+			}
+		}
+		if !isDigit {
+			return false
+		}
+	}
+	return true
+}
+
+/* Instead of a + b in Python, use a.Add(b) in Go */
+func (p* PyString) Add(text string) string {
+	p.text += text
+	return p.text
+}
+
+/* Append a string */
+func (p* PyString) Append(s string) {
+	p.text += s
+}
+
+/* Remove the last occurence of a string */
+func (p* PyString) Subtract(text string) string {
+	pos := p.RFind(text)
+	if pos != -1 {
+		p.text = p.text[:pos] + p.text[pos + len(text):]
+	}
+	return p.text
+}
+
+/* Find the last occurance */
+func (p* PyString) RFind(text string) int {
+	return strings.LastIndex(p.text, text)
+}
+
+func (p *PyString) Split(sep string) []string {
+	return strings.Split(p.text, sep)
+}
+
+func (p *PyString) Multiply(n int) string {
+	var buf bytes.Buffer
+	for i := 0; i < n; i++ {
+		buf.WriteString(p.text)
+	}
+	return buf.String()
 }
--- pystring_test.go
+++ pystring_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestCapitalize(t *testing.T) {
 	const in, out = "hello", "Hello"
-	result := New(in).Capitalize().s
+	result := New(in).Capitalize().Get()
 	if out != result {
 		t.Errorf("Capitalized version of %v did not become %v, but %v!\n", in, out, result)
 	}
@@ -12,7 +12,7 @@ func TestCapitalize(t *testing.T) {
 
 func TestStrip(t *testing.T) {
 	const in, out = "    \n\n\nhello\t\t\n\n\n", "hello"
-	result := New(in).Strip().s
+	result := New(in).Strip().Get()
 	if out != result {
 		t.Errorf("Stripped version of %v did not become %v, but %v!\n", in, out, result)
 	}
@@ -33,3 +33,83 @@ func TestIndex(t *testing.T) {
 		t.Errorf("Index of bb in %v, was not %v but %v!\n", in, out, result)
 	}
 }
+
+func TestEndsWith(t *testing.T) {
+	const in1, in2 = "aabbcc", "abcdef"
+	if New(in1).EndsWith("cc") == false {
+		t.Errorf("EndsWith: %v does end with cc!\n", in1)
+	}
+	if New(in2).EndsWith("dd") == true {
+		t.Errorf("EndsWith: %v does not end with dd!\n", in2)
+	}
+}
+
+func TestStartsWith(t *testing.T) {
+	const in1, in2 = "aabbcc", "abcdef"
+	if New(in1).StartsWith("cc") != false {
+		t.Errorf("StartsWith: %v does not start with cc!\n", in1)
+	}
+	if New(in2).StartsWith("ab") != true {
+		t.Errorf("StartsWith: %v does start with ab!\n", in2)
+	}
+}
+
+func TestFind(t *testing.T) {
+	const in = "aabbcc"
+	if New(in).Find("cc") != 4 {
+		t.Errorf("Find: %v has cc in position 4!\n", in)
+	}
+	if New(in).Find("cd") != -1 {
+		t.Errorf("Find: %v does not have cd!\n", in)
+	}
+}
+
+func TestIsDigit(t *testing.T) {
+	const in1, in2 = "223098473", "x1z"
+	if New(in1).IsDigit() != true {
+		t.Errorf("IsDigit: %v should be true!\n", in1)
+	}
+	if New(in2).IsDigit() != false {
+		t.Errorf("IsDigit: %v should be false!\n", in2)
+	}
+	if New("").IsDigit() != false {
+		t.Errorf("IsDigit: empty string should be false!\n")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	a := New("abc")
+	if a.Add("cde") != "abccde" {
+		t.Errorf("abc + cde should be abccde\n")
+	}
+}
+
+func TestSubstract(t *testing.T) {
+	a := New("abcdef")
+	if a.Subtract("def") != "abc" {
+		t.Errorf("abcdef - def should be abc\n")
+	}
+	a = New("ost kake ost kake ost")
+	if a.Subtract("kake") != "ost kake ost  ost" {
+		t.Errorf("ost kake ost kake ost - kake should be ost kake ost  ost\n")
+	}
+}
+
+func TestSplit(t *testing.T) {
+	a := New("a:b:c")
+	if a.Split(":")[1] != "b" {
+		t.Errorf("a:b:c split on : should be a b c\n")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	a := New("a")
+	if a.Multiply(5) != "aaaaa" {
+		t.Errorf("a*5 should be aaaaa\n")
+	}
+	b := New("ost")
+	if b.Multiply(2) != "ostost" {
+		t.Errorf("ost*2 should be ostost\n")
+	}
+}
+
